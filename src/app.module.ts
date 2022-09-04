322a235701b79de80f412e49022f35735bf6ed38
natsunsy <rlimachip@uni.pe>
2022-09-04 16:55:03 -0500

    feat: add vercel config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,11 @@ import { AuthModule } from './auth/auth.module';
 import { RadioModule } from './radio/radio.module';
 import { PodcastModule } from './podcast/podcast.module';
 
-const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: !ENV || ENV == 'production' ? '.env' : `.env.${ENV}`,
+      envFilePath: ['.env.development.local'],
     }),
     MongooseModule.forRoot(process.env.MONGO_ATLAS_CONNECTION),
     AuthModule,
